99f7ef3c014135d0928a02232abfa2cb05ca16a5
gipeio <gipeio22@gmail.com>
2024-07-11 10:35:14 +0200

    ✨ firstCommit

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+// Import your reducers here
+// import userReducer from './userReducer';
+// import tasksReducer from './tasksReducer';
+
+const rootReducer = combineReducers({
+  // user: userReducer,
+  // tasks: tasksReducer,
+});
+
+export default rootReducer;
